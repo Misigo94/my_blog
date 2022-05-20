4d73ac8827fedbd99c1d6c33f718258b82c699ad
Misigomartin <Misigomartin@gmail.com>
2022-05-20 22:57:00 +0300

    add request file

--- app/request.py
+++ app/request.py
@@ -1,7 +1,41 @@
-# import requests
 
-# def random_post():
-#     url='http://quotes.stormconsultancy.co.uk/random.json'
-#     pos= requests.get(url)
-#     posts= pos.json()
-#     return posts
+import urllib.request
+import requests
+import json
+from .models import Blogg
+
+#  stations = json.loads(response.text)
+#   stations_by_name = []
+#   for station in stations:
+#     name = station.get('name')
+#     url = station.get('url')
+#     favicon = station.get('favicon')
+#     votes = station.get('votes')
+#     if station:
+#       obj = Radio(name=name, url=url, favicon=favicon, votes=votes)
+#       stations_by_name.append(obj)
+#   return stations_by_name
+def get_blog():
+
+    url = "https://quotes15.p.rapidapi.com/quotes/random/"
+
+    headers = {
+        "X-RapidAPI-Host": "quotes15.p.rapidapi.com",
+        "X-RapidAPI-Key": "31e9cbe3d8msh574eaee9cfc96afp14b0aejsn2dbf23758e92"
+    }
+
+    response = requests.request("GET", url, headers=headers)
+    results = json.loads(response.text)
+    blogs = results['originator']
+
+    bloglist=[]
+    for blog in blogs:
+        id = blog.get('id')
+        category = blog.get('category')
+        text = blog.get('text')
+        author = blog.get('author')
+        if blog:
+            obj = Blogg(id=id,category=category,text=text,author=author)
+        bloglist.append(obj)
+    # print(blog)
+    return bloglist

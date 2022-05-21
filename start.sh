1773fc942827505da54dd0a5b79d7273cd06c1a3
Misigomartin <Misigomartin@gmail.com>
2022-05-21 09:49:11 +0300

    added a start file

--- start.sh
+++ start.sh
@@ -3,7 +3,9 @@ export MAIL_PASSWORD=Misigomart94
 export SECRET_KEY='1234'
 export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://martin3:password@localhost:5432/blogdb'
 
-#  python3.9 manage.py migrate
+# python3 manage.py db init
+# python3 manage.py db migrate -m "second migration"
+# python3 manage.py db upgrade
 # python3.9 manage.py shell
 python3.9 manage.py server
 
